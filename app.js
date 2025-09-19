aa1610458627a7d2b8455c61d17aa8d0904c9bdd
idempomlieko <matus.durac.0@gmail.com>
2025-09-19 08:22:30 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const btn = document.getElementById('red-btn');
+	const video = document.getElementById('surprise-video');
+    const overlay = document.getElementById('flash-overlay');
+    const btnImg = document.getElementById('btn-img');
+	let colorInterval = null;
+	btn.addEventListener('mousedown', function() {
+		btnImg.src = 'static/button_pressed.png';
+		video.style.display = 'block';
+		video.classList.add('bg-video');
+		video.loop = true;
+		video.currentTime = 0;
+		video.volume = 1.0;
+		video.play();
+		if (!colorInterval) {
+			colorInterval = setInterval(() => {
+				overlay.style.background = `rgba(${rand255()},${rand255()},${rand255()},0.7)`;
+			}, 150);
+		}
+	});
+
+	btn.addEventListener('mouseup', stopMedia);
+	btn.addEventListener('mouseleave', stopMedia);
+	btn.addEventListener('touchend', stopMedia);
+	btn.addEventListener('touchcancel', stopMedia);
+
+	function stopMedia() {
+		btnImg.src = 'static/button_unpressed.png';
+		video.pause();
+		video.loop = false;
+		video.style.display = 'none';
+		video.classList.remove('bg-video');
+		if (colorInterval) {
+			clearInterval(colorInterval);
+			colorInterval = null;
+			overlay.style.background = 'transparent';
+		}
+	}
+
+	function rand255() {
+		return Math.floor(Math.random() * 256);
+	}
+});
